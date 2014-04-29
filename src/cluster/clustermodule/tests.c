009f52310928f856ec74828d3ddf800ff3849b4e
root <root@daisy-X450VC.(none)>
2014-04-30 00:38:54 +0800

    first commit

---
+++ src/cluster/clustermodule/tests.c
@@ -0,0 +1,29 @@
+/*
+ * tests.c
+ *
+ *  Created on: Apr 22, 2014
+ *      Author: daisy
+ */
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#include "cluster.h"
+//
+//int main() {
+//	char *clustername = "MyCluster";
+//	cluster* _cluster = initialcluster(clustername);
+//	clusteraddnode(_cluster, "Group1 Group2 Group3");
+//	addnodechild(_cluster, "127.0.0.1:6379", "Group1");
+//	addnodechild(_cluster, "127.0.0.1:6380", "Group2");
+//	addnodechild(_cluster, "127.0.0.1:6381", "Group3");
+//
+//	char key[128]="";
+//	int i;
+//	for (i = 0; i < 10; i++) {
+//		sprintf(key, "key%d", i);
+//		printf("%s is in: %s\n", key, getserver(_cluster, key));
+//	}
+//	return 0;
+//}
+
